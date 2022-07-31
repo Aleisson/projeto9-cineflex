6f2cfa98a4deeb216bc67cd8c6a11d97bbae3867
Aleisson <caioaleisson@gmail.com>
2022-07-30 21:13:54 -0300

    App

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function App() {
+
+    
+
+    return (
+    
+        <div class="content">
+            <h1>Ola Tudo bem?</h1>
+        </div>
+    
+    );
+
+}
+
+export default App;
