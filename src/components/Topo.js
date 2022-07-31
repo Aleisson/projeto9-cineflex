07b2ff1da1f53eae4c994c8686d955f7a864f4c6
Aleisson <caioaleisson@gmail.com>
2022-07-31 00:45:55 -0300

    Depedencies

--- src/components/Topo.js
+++ src/components/Topo.js
@@ -1,10 +1,10 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 function Topo() {
 
     return (
         <Top>
-
+            <p>CINEFLEX</p>
         </Top>
     );
 
@@ -14,8 +14,26 @@ export default Topo;
 
 const Top = styled.div`
 
-	width: 100px;
-	height: 100px;
-	background-color: red;
+    position: absolute;
+    width: 100%;
+    height: 100px;
+    left: 0px;
+    top: 0px;
+    background: #C3CFD9;
+    display: flex;
+    justify-content: center;
+    align-items:center;
+   
+    p{
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 34px;
+        line-height: 40px;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        color: #E8833A;
+    }
 
 `;
